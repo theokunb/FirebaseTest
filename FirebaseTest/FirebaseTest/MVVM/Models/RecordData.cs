65a6ba58438818e6a305612e99b383fa77d3593b
theokunb <makarovalexeyvery@gmail.com>
2022-01-15 12:34:13 +0900

    Добавьте файлы проекта.

---
+++ FirebaseTest/FirebaseTest/MVVM/Models/RecordData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirebaseTest.MVVM.Models
+{
+    public class RecordData
+    {
+        public string Text { get; set; }
+    }
+}
---
+++ FirebaseTest/FirebaseTest/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using FirebaseTest.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirebaseTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<RecordData> RecordCollection { get; set; } = new ObservableCollection<RecordData>();
+        private FirebaseClient client = new FirebaseClient("https://fir-test-7a840-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            IDisposable collection = client.Child("Records").AsObservable<RecordData>().Subscribe((dbEvent) =>
+            {
+                if (dbEvent.Object != null)
+                    RecordCollection.Add(dbEvent.Object);
+            });
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            client.Child("Records").PostAsync(new RecordData 
+            {
+                Text = recordEntry.Text
+            });
+            recordEntry.Text = string.Empty;
+        }
+    }
+}
